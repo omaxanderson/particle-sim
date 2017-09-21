519a74f489d354c3db93b2ce71b11f5f6c99f8d6
Max Anderson <Max@maxs-macbook-pro-3.local>
2017-09-21 10:24:31 -0400

    Initial Commit

---
+++ std_test2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  std_test2
+//
+//  Created by Max Anderson on 9/21/17.
+//  Copyright © 2017 Max Anderson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
